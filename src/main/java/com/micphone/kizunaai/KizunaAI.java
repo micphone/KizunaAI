d32c31ce576672ad379a8671d1db5c02b7e936cf
micphone <2082183611@qq.com>
2024-04-24 19:33:30 +0800

    Initial commit

---
+++ src/main/java/com/micphone/kizunaai/KizunaAI.java
@@ -0,0 +1,28 @@
+package com.micphone.kizunaai;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import org.slf4j.Logger;
+
+@Mod(KizunaAI.MOD_ID)
+public class KizunaAI {
+    public static final String MOD_ID = "kizunaai";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public KizunaAI(IEventBus modEventBus) {
+        modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+}
